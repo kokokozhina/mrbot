dd7f4e80fe99d6baf9fa5728500ca82a7dddf425
Olga Kozhina <kozhina.olga.1997@gmail.com>
2018-05-21 22:34:58 +0300

    java docs to interface

--- git-client-api/src/main/java/git/client/api/GitClientApi.java
+++ git-client-api/src/main/java/git/client/api/GitClientApi.java
@@ -3,10 +3,36 @@ package git.client.api;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * This interface provides methods to implement
+ * if you want to add a git server instance
+ * in Merge Request Bot
+ * @author kokokozhina
+ */
 public interface GitClientApi {
+
+    /**
+     * Returns a list of titles of groups in the git server instance
+     * @return {@code List<String>} is a list of titles of groups in the git server instance
+     * @throws IOException if connection is invalid
+     */
     List<String> getGroups() throws IOException;
 
+
+    /**
+     * Returns a list of titles of projects that belongs to certain group
+     * @param group title of the group
+     * @return {@code List<String>} is a list of titles of projects that belongs to certain group
+     * @throws IOException if connection is invalid
+     */
     List<String> getProjectsByGroupName(String group) throws IOException;
 
+    /**
+     * Returns a list of titles of merge requests that belongs to certain project from the group
+     * @param group title of the group
+     * @param project title of the project
+     * @return {@code List<String>} is a list of titles of merge requests that belongs to certain project from the group
+     * @throws IOException if connection is invalid
+     */
     List<String> getMrsByGroupAndProject(String group, String project) throws IOException;
 }
--- msgr-client-api/src/main/java/msgr/client/api/MsgrClientApi.java
+++ msgr-client-api/src/main/java/msgr/client/api/MsgrClientApi.java
@@ -3,9 +3,26 @@ package msgr.client.api;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * This interface provides methods to implement if you wand to add a messenger to Merge Request Bot
+ * @author kokokozhina
+ */
 public interface MsgrClientApi {
 
+
+    /**
+     * Returns a list of titles of channels in a messenger workspace
+     * @return {@code List<String>} is a list of titles of channels that belongs to messenger workspace
+     * @throws IOException
+     */
     List<String> getChannels() throws IOException;
 
+
+    /**
+     * Post a message to a slack channel
+     * @param message message to send
+     * @param s channel name
+     * @throws IOException
+     */
     void postToSlackByChannelName(String message, String s) throws IOException;
 }
